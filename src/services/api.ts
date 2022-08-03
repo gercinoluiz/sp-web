2f6ff7c7be41f15d950935e71554ab369bc422d3
Gercino Luiz <gercino.neto.luiz@prodam.sp.gov.br>
2022-08-03 17:06:04 -0300

    fc

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-     baseURL: import.meta.env.VITE_MOCK_ENDPOINT
+     baseURL: import.meta.env.VITE_ENDPOINT
      // headers: {
      //      'Cache-Control': 'no-cache',
      //      Pragma: 'no-cache',
